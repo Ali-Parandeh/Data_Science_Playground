3a2338646bc861778f2f3a7bc8f9e473b810febf
Ali Parandeh <43654026+Ali-Parandeh@users.noreply.github.com>
2019-12-02 01:42:18 +0000

    Add assignments

---
+++ Datacamp Assignments/Data Engineer Track/3. Software Engineering & Data Science/26_using_docstests.py
@@ -0,0 +1,30 @@
+import doctest
+from collections import Counter
+
+def sum_counters(counters):
+    """Aggregate collections.Counter objects by summing counts
+
+    :param counters: list/tuple of counters to sum
+    :return: aggregated counters with counts summed
+
+    >>> d1 = text_analyzer.Document('1 2 fizz 4 buzz fizz 7 8')
+    >>> d2 = text_analyzer.Document('fizz buzz 11 fizz 13 14')
+    >>> sum_counters([d1.word_counts, d2.word_counts])
+    Counter({'buzz': 2, 'fizz': 4})
+    """
+    return sum(counters, Counter())
+
+doctest.testmod()
+
+
+'''
+<script.py> output:
+    **********************************************************************
+    File "__main__", line 9, in NoName
+    Failed example:
+        sum_counters([d1.word_counts, d2.word_counts])
+    Expected:
+        Counter({'buzz': 2, 'fizz': 4})
+    Got:
+        Counter({'fizz': 4, 'buzz': 2})
+'''
