06afc92e89cdb5b5bd6642d25660f88a5402cd16
Ali Parandeh <Ali.Parandeh@outlook.com>
2019-12-18 00:54:57 +0000

    Add SQL table manipulation assignments

---
+++ Datacamp Assignments/Data Engineer Track/9. Introduction to Relational Databases in SQL/1_SELECT.sql
@@ -0,0 +1,17 @@
+-- Query the right table in information_schema
+SELECT * 
+FROM information_schema.tables
+-- Specify the correct table_schema value
+WHERE table_schema = 'public';
+
+
+-- Query the right table in information_schema to get columns
+SELECT column_name, data_type 
+FROM information_schema.columns 
+WHERE table_name = 'university_professors' AND table_schema = 'public';
+
+
+-- Query the first five rows of our table
+SELECT * 
+FROM university_professors 
+LIMIT 5;
