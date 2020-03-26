871ca3ce748d8d065c43e92c9f0b0ad7f361bb04
Ali Parandeh <Ali.Parandeh@outlook.com>
2020-03-26 11:09:33 +0000

    Add random circles masterpiece

--- pyimagesearch gurus/Module 1/lesson_1_3/drawing.py
+++ pyimagesearch gurus/Module 1/lesson_1_3/drawing.py
@@ -34,3 +34,32 @@ blue = (255, 0, 0)
 cv2.rectangle(canvas, (200,50), (225, 125), blue, -1)
 cv2.imshow('Canvas', canvas)
 cv2.waitKey(0)
+
+# reset our canvas and draw a white circle at the center of the canvas with
+# increasing radii - from 25 pixels to 150 pixels
+canvas = np.zeros((300, 300, 3), dtype='uint8')
+(centreX, centreY) = (canvas.shape[1] // 2, canvas.shape[0] // 2)
+white = (255, 255, 255)
+
+for r in range(0, 175, 25):
+    cv2.circle(canvas, (centreX, centreY), r, white)
+
+# show our work of art
+cv2.imshow('Canvas', canvas)
+cv2.waitKey(0)
+
+# let's go crazy and draw 25 random circles
+for i in range(0, 25):
+    # randomly generate a radius size between 5 and 200, generate a random
+    # color, then pick a random point on our canvas where the circle
+    # will be drawn
+    radius = np.random.randint(5, high=200)
+    color = np.random.randint(0 , high=256, size=(3, )).tolist()
+    pt = np.random.randint(0 , high=300, size=(2,))
+
+    # draw our random circle
+    cv2.circle(canvas, tuple(pt), radius, color, -1)
+
+# show our masterpiece
+cv2.imshow('Canvas', canvas)
+cv2.waitKey(0)
