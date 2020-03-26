33e868d58ec2164e1f920e5d8aeed7b429bc1648
Ali Parandeh <Ali.Parandeh@outlook.com>
2020-03-26 11:23:27 +0000

    Add circles and rectangles around his face

--- pyimagesearch gurus/Module 1/lesson_1_3/drawing.py
+++ pyimagesearch gurus/Module 1/lesson_1_3/drawing.py
@@ -63,3 +63,17 @@ for i in range(0, 25):
 # show our masterpiece
 cv2.imshow('Canvas', canvas)
 cv2.waitKey(0)
+
+# load the image of Adrian in Florida
+image = cv2.imread('../img/florida_trip.png')
+
+# draw the circle around his face, two filled in circles covering his eyes and
+# a rectangle surrounding his mouth
+cv2.circle(image, (168, 188), 90, (0, 0 , 255), 2)
+cv2.circle(image, (150, 164), 10, (0, 0 , 255), -1)
+cv2.circle(image, (192, 174), 10, (0, 0 , 255), -1)
+cv2.rectangle(image, (134, 200), (186, 218), (0, 0 , 255), -1)
+
+# show the output image
+cv2.imshow('Output', image)
+cv2.waitKey(0)
