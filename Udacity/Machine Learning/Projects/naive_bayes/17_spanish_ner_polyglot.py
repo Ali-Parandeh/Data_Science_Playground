f718eb287f2feb157a576db7eb65cde74dc3bb2e
Ali Parandeh <Ali.Parandeh@outlook.com>
2020-03-28 17:20:55 +0000

    Delete junk

--- Udacity/Machine Learning/Projects/naive_bayes/17_spanish_ner_polyglot.py
+++
@@ -1,8 +0,0 @@
-# Calculate the proportion of txt.entities that
-# contains 'Márquez' or 'Gabo': prop_ggm
-count =0
-for ent in txt.entities:
-    if ('Márquez' in ent or 'Gabo' in ent):
-        count += 1
-        
-prop_ggm = count / len(txt.entities)
